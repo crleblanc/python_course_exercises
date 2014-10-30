6694a7929cc1c1c3d307356cc320902dd06f6380
Chris LeBlanc <crleblanc@gmail.com>
2014-10-30 22:25:44 +0000

    Opps, now checking any words begining with i or I.

--- 1.2_basics/parse_zen.py
+++ 1.2_basics/parse_zen.py
@@ -2,7 +2,7 @@
 # Note: the line above tells the shell how to run this file.  Not mandatory but recommended.
 
 # Exercise 1: Parse each word in the string called zen (already created below as a multiline string)
-# and print out all words that start with the letter "i".
+# and print out all words that start with the letter "i" or "I".
 #
 # Hints: Use string.split() to split each word, and use a for 
 # loop to loop over them testing the first character of each word.
