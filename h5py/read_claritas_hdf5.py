605d03ce9c9ede04b4abc6c7c52980ca90c83345
Chris LeBlanc <crleblanc@gmail.com>
2014-11-04 17:09:20 +1300

    added an h5py example dir with a single shot and script to read and display it.

---
+++ h5py/read_claritas_hdf5.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Read in a Claritas format HDF5 file and see what's there.  GLOBE Claritas
+# uses HDF5 for it's internal data format.  Open an example file and look at the
+# data and trace headers.
+
+import numpy as np
+import h5py
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def main():
+    with h5py.File('test_shot.h5', 'r') as h5file:
+
+        # quick access to objects that act like NumPy arrays, but only read from disk when necessary
+        trace_data = h5file['/TRACE_DATA/DEFAULT/data_array']
+        trace_headers = h5file['/TRACE_DATA/DEFAULT/trace_headers']
+
+        # Make a Pandas object out of the 'record array' like object from h5py
+        headers_dframe = pd.DataFrame(np.array(trace_headers))
+
+        print headers_dframe
+
+        # display the amplitudes
+        trace_array = np.array(trace_data)
+        plt.imshow(trace_array.T, cmap=plt.cm.gray, aspect=0.05)
+        plt.show()
+
+if __name__ == '__main__':
+    main()
